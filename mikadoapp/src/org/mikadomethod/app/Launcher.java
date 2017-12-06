1a9baafba9760395e6253d3cd6039d89f09f55d0
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-06 11:54:47 -0300

    First project using Mikado method.

--- mikadoapp/src/org/mikadomethod/app/Launcher.java
+++ mikadoapp/src/org/mikadomethod/app/Launcher.java
@@ -5,6 +5,8 @@
  */
 package org.mikadomethod.app;
 
+import org.mikado.ui.App;
+import org.mikado.ui.ApplicationException;
 /**
  *
  * @author barcvilla
@@ -12,6 +14,22 @@ package org.mikadomethod.app;
 public class Launcher 
 {
     public static void main(String[] args) {
-        
+        try
+        {
+            /**
+             * 1. Refactoring: Aqui tenemos una ubicacion hard-coded
+             * queremos evitar la ruta hardcoded y conseguir esto pero sin romper el codido.
+            
+            App.setStorageFile("opt/local/app/db.txt");
+            */
+            App.setStorageFile(args[0]); //inicio de al app configurado via parametro command line.
+            App app = new App();
+            app.launch();
+        }
+        catch(ApplicationException ex)
+        {
+            System.err.println("Could not start the app");
+            ex.printStackTrace();
+        }
     }
 }
